bf4f5bb79e4c207ea2be6539d102bd82e57aca03
MarlonDSC <marlonsubuyu.2012@gmail.com>
2022-05-16 17:39:42 -0400

    upload project to GitHub

---
+++ lib/utils/firebase_api_key.dart
@@ -0,0 +1,7 @@
+const apiKey = "AIzaSyBg_VfSXzNn1fp8hmPHud3y2mGX66gv5-E";
+const authDomain = "flutter-instagram-cl0ne.firebaseapp.com";
+const projectId = "flutter-instagram-cl0ne";
+const storageBucket = "flutter-instagram-cl0ne.appspot.com";
+const messagingSenderId = "387693602605";
+const appId = "1:387693602605:web:b19c374dd608fe02c815a4";
+const measurementId = "G-SWQ2THSM8S";
